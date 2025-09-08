0a3fc7da533fb61cd9a3c3524f35feca936f9d5d
jashwanth-cse <26jashwanth@gmail.com>
2025-09-08 12:19:52 +0530

    added website open feature

--- src/main/java/com/smartroutine/model/WebAction.java
+++ src/main/java/com/smartroutine/model/WebAction.java
@@ -1,16 +1,35 @@
 package com.smartroutine.model;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class WebAction extends Action {
-    private String url;
+    private final String url;
 
     public WebAction(String name, String url) {
         super(name);
-        this.url = url;
+        // Ensure URL starts with http:// or https://
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            this.url = "https://" + url;
+        } else {
+            this.url = url;
+        }
     }
 
     @Override
     public void performAction() {
         System.out.println("Opening website: " + url);
-        // Later: Desktop.getDesktop().browse(new URI(url));
+        try {
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI(url));
+            } else {
+                System.out.println("Cannot open browser on this system.");
+            }
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Failed to open URL: " + url);
+            e.printStackTrace();
+        }
     }
 }
